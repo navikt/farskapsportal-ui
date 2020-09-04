20bf2955591738cf6c1b07593d908fccf03c2a80
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2020-09-04 10:32:12 +0200

    Test env variables

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -7,7 +7,9 @@ function Banner() {
     return (
         <div className="Banner">
             <Sidetittel>
-                <FormattedMessage id="page.title" />
+                <FormattedMessage id="page.title" /> hallo{' '}
+                {process.env.REACT_APP_LOGINSERVICE_URL}{' '}
+                {JSON.stringify(process.env.REACT_APP_LOGINSERVICE_URL)}
             </Sidetittel>
         </div>
     );
