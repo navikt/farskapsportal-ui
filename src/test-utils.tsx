812a6556b100874846731a7bd7a68fedad95427b
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2020-11-12 17:03:00 +0100

    Add option to pass store to tests and add test for Soeknad

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,16 +1,22 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-axe/extend-expect';
-import React, { ComponentType, ReactElement, ReactNode } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render, RenderOptions } from '@testing-library/react';
 
 import IntlProvider from 'intl/IntlProvider';
 import { StoreProvider } from 'store/Context';
-import { initialState, reducer } from 'store/store';
+import { initialState, reducer, Store } from 'store/store';
 
-const AllTheProviders = ({ children }: { children: ReactNode }): ReactElement => {
+const AllTheProviders = ({
+    children,
+    store,
+}: {
+    children: ReactNode;
+    store?: Partial<Store>;
+}): ReactElement => {
     return (
-        <StoreProvider initialState={initialState} reducer={reducer}>
+        <StoreProvider initialState={{ ...initialState, ...store }} reducer={reducer}>
             <IntlProvider>
                 <Router>{children}</Router>
             </IntlProvider>
@@ -18,8 +24,10 @@ const AllTheProviders = ({ children }: { children: ReactNode }): ReactElement =>
     );
 };
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
-    render(ui, { wrapper: AllTheProviders as ComponentType, ...options });
+const customRender = (
+    ui: ReactElement,
+    { store, ...options }: RenderOptions & { store?: Partial<Store> } = {}
+) => render(<AllTheProviders store={store}>{ui}</AllTheProviders>, { ...options });
 
 export * from '@testing-library/react';
 
