12ef047b887bae19ba3ffb874fabce4ae1bb1cce
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2020-11-13 12:43:41 +0100

    Change test setup

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -8,13 +8,7 @@ import IntlProvider from 'intl/IntlProvider';
 import { StoreProvider } from 'store/Context';
 import { initialState, reducer, Store } from 'store/store';
 
-const AllTheProviders = ({
-    children,
-    store,
-}: {
-    children: ReactNode;
-    store?: Partial<Store>;
-}): ReactElement => {
+function ProviderWrapper({ children, store }: { children: ReactNode; store?: Partial<Store> }) {
     return (
         <StoreProvider initialState={{ ...initialState, ...store }} reducer={reducer}>
             <IntlProvider>
@@ -22,12 +16,12 @@ const AllTheProviders = ({
             </IntlProvider>
         </StoreProvider>
     );
-};
+}
 
 const customRender = (
     ui: ReactElement,
     { store, ...options }: RenderOptions & { store?: Partial<Store> } = {}
-) => render(<AllTheProviders store={store}>{ui}</AllTheProviders>, { ...options });
+) => render(<ProviderWrapper store={store}>{ui}</ProviderWrapper>, { ...options });
 
 export * from '@testing-library/react';
 
