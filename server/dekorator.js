d9b1f3448f8edca177228c88383fed5487050435
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2021-03-17 13:08:20 +0100

    Cleanup and fix running server on localhost

--- server/dekorator.js
+++ server/dekorator.js
@@ -1,11 +1,9 @@
 import { injectDecoratorServerSide } from '@navikt/nav-dekoratoren-moduler/ssr/index.js';
 
-console.log('process.env.ENV');
-console.log(process.env.ENV);
-
 export const getHtmlWithDekorator = (filePath) =>
     injectDecoratorServerSide({
         env: process.env.ENV,
+        port: process.env.ENV === 'localhost' ? 8100 : undefined,
         filePath: filePath,
         level: 'Level4',
         redirectToApp: true,
--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 import 'dotenv/config.js';
 import express from 'express';
-// import mustacheExpress from 'mustache-express';
 import cookieParser from 'cookie-parser';
 import fetch from 'node-fetch';
 import compression from 'compression';
-// import { getDecorator } from './decorator.js';
 import { getHtmlWithDekorator } from './dekorator.js';
 
 const buildPath = '../build';
@@ -13,11 +11,6 @@ const tokenName = 'selvbetjening-idtoken';
 const app = express();
 
 app.use(compression());
-
-// app.set('views', buildPath);
-// app.set('view engine', 'mustache');
-// app.engine('html', mustacheExpress());
-
 app.use(cookieParser());
 
 // Parse application/json
@@ -101,19 +94,6 @@ app.post('/api/farskapserklaering/ny', async (req, res) => {
 
 // Match everything except internal og static
 app.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) =>
-    // getDecorator()
-    //     .then((fragments) => {
-    //         res.render('index.html', {
-    //             ...fragments,
-    //             LOGIN_URL: process.env.LOGINSERVICE_URL,
-    //             APP_VERSION: process.env.APP_VERSION,
-    //         });
-    //     })
-    //     .catch((e) => {
-    //         const error = `Failed to get decorator: ${e}`;
-    //         console.error(error);
-    //         res.status(500).send(error);
-    //     })
     getHtmlWithDekorator(`${buildPath}/index.html`)
         .then((html) => {
             res.send(
