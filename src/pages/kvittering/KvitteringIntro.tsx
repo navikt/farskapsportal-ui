6374ae4aae0c505576f4ffad7fc8c9e82f6e1fe8
Gorvell de Lichtenberg, Frederik <frederik.gorvell-de-lichtenberg@nav.no>
2021-03-24 18:42:20 +0100

    Add get redirect url for far

--- src/pages/kvittering/KvitteringIntro.tsx
+++ src/pages/kvittering/KvitteringIntro.tsx
@@ -2,11 +2,10 @@ import { Success } from '@navikt/ds-icons';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import DatePresentation from 'components/date-presentation/DatePresentation';
 import ExternalLink from 'components/external-link/ExternalLink';
 import { Farskapserklaering } from 'types/farskapserklaering';
 import { isBrukerFar } from 'utils/farskapserklaering';
-import { getMessage } from 'utils/intl';
+import { formatDate, getMessage } from 'utils/intl';
 
 import './KvitteringIntro.less';
 
@@ -18,11 +17,13 @@ function KvitteringIntro({ erklaering }: KvitteringIntroProps) {
     return (
         <section className="KvitteringIntro">
             <Success id="success-icon" aria-label="Success icon" role="img" focusable={false} />
-            {isBrukerFar(erklaering) ? (
-                <KvitteringIntroFar erklaering={erklaering} />
-            ) : (
-                <KvitteringIntroMor erklaering={erklaering} />
-            )}
+            <div>
+                {isBrukerFar(erklaering) ? (
+                    <KvitteringIntroFar erklaering={erklaering} />
+                ) : (
+                    <KvitteringIntroMor erklaering={erklaering} />
+                )}
+            </div>
         </section>
     );
 }
@@ -30,41 +31,98 @@ function KvitteringIntro({ erklaering }: KvitteringIntroProps) {
 function KvitteringIntroMor({ erklaering }: { erklaering: Farskapserklaering }) {
     const intl = useIntl();
 
-    return (
-        <div>
+    const farSignert = (signertAvFar: string) => (
+        <>
             <Normaltekst>
-                <FormattedMessage id="kvittering.intro.mor.1" />
-                {erklaering.dokument?.signertAvMor && (
+                <FormattedMessage
+                    id="kvittering.intro.mor.farSignert"
+                    values={{ date: formatDate(intl, signertAvFar) }}
+                />{' '}
+                {erklaering.barn?.termindato ? (
                     <>
-                        {' '}
-                        <DatePresentation date={erklaering.dokument.signertAvMor} />
+                        <FormattedMessage
+                            id="kvittering.intro.termindato"
+                            values={{ termindato: formatDate(intl, erklaering.barn?.termindato) }}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <FormattedMessage
+                            id="kvittering.intro.foedselsnummer"
+                            values={{ fnr: erklaering.barn?.foedselsnummer ?? '' }}
+                        />
                     </>
                 )}
-                .
             </Normaltekst>
             <Normaltekst>
-                <FormattedMessage id="kvittering.intro.mor.2" />
-            </Normaltekst>
-            <Normaltekst>
-                <FormattedMessage id="kvittering.intro.mor.3" />
+                <FormattedMessage id="kvittering.intro.mor.4" />
                 <ExternalLink href={getMessage(intl, 'kvittering.intro.mor.link')} />
             </Normaltekst>
-        </div>
+        </>
     );
+
+    const farIkkeSignert = () => {
+        return (
+            <>
+                <Normaltekst>
+                    <FormattedMessage
+                        id="kvittering.intro.mor.1"
+                        values={{ date: formatDate(intl, erklaering.dokument?.signertAvMor ?? '') }}
+                    />
+                </Normaltekst>
+                <Normaltekst>
+                    <FormattedMessage id="kvittering.intro.mor.2" />
+                </Normaltekst>
+                <Normaltekst>
+                    <FormattedMessage id="kvittering.intro.mor.3" />
+                </Normaltekst>
+                <Normaltekst>
+                    <FormattedMessage id="kvittering.intro.mor.4" />
+                    <ExternalLink href={getMessage(intl, 'kvittering.intro.mor.link')} />
+                </Normaltekst>
+            </>
+        );
+    };
+
+    return erklaering.dokument?.signertAvFar
+        ? farSignert(erklaering.dokument.signertAvFar)
+        : farIkkeSignert();
 }
 
 function KvitteringIntroFar({ erklaering }: { erklaering: Farskapserklaering }) {
+    const intl = useIntl();
+
     return (
-        <Normaltekst>
-            <FormattedMessage id="kvittering.intro.far" />
-            {erklaering.dokument?.signertAvFar && (
-                <>
-                    {' '}
-                    <DatePresentation date={erklaering.dokument.signertAvFar} />
-                </>
-            )}
-            .
-        </Normaltekst>
+        <>
+            <Normaltekst>
+                <FormattedMessage
+                    id="kvittering.intro.far.1"
+                    values={{ date: formatDate(intl, erklaering.dokument?.signertAvFar ?? '') }}
+                />{' '}
+                {erklaering.barn?.termindato ? (
+                    <>
+                        <FormattedMessage
+                            id="kvittering.intro.termindato"
+                            values={{ termindato: formatDate(intl, erklaering.barn?.termindato) }}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <FormattedMessage
+                            id="kvittering.intro.foedselsnummer"
+                            values={{ fnr: erklaering.barn?.foedselsnummer ?? '' }}
+                        />
+                    </>
+                )}
+            </Normaltekst>
+            <Normaltekst>
+                <FormattedMessage id="kvittering.intro.far.2" />
+            </Normaltekst>
+            <Normaltekst>
+                <FormattedMessage id="kvittering.intro.far.3" />
+                <ExternalLink href={getMessage(intl, 'kvittering.intro.far.link')} />
+            </Normaltekst>
+        </>
     );
 }
 
--- src/texts/nb.ts
+++ src/texts/nb.ts
@@ -177,11 +177,19 @@ export default {
     'skjema.far.confirm.readAndUnderstood.label': 'Jeg har lest og forstått hva farksapserklæringen innebærer.',
     'skjema.far.confirm.cannotWithdraw.label': 'Jeg er kjent med at denne bekreftelsen ikke kan trekkes tilbake på et senere tidspunkt.',
 
-    'kvittering.intro.mor.1': 'Vi har mottat dine opplysninger om far',
+    'kvittering.intro.foedselsnummer': 'med fødselsnummer {fnr}.',
+    'kvittering.intro.termindato': 'forventet født {termindato}.',
+    'kvittering.intro.mor.1': 'Vi har mottatt dine opplysninger om far {date}.',
     'kvittering.intro.mor.2': 'Erklæringen vil bli videresendt til han for godkjenning og signering.',
-    'kvittering.intro.mor.3': 'Du kan ikke endre opplysningene du har gitt. Du kan gjenfinne erklæringene her',
-    'kvittering.intro.mor.link': '[lenke]',
-    'kvittering.intro.far': 'Du har signert farskapserklæring',
+    'kvittering.intro.mor.3': 'Du kan ikke endre opplysningene du har gitt.',
+    'kvittering.intro.mor.4': 'Du kan gjenfinne erklæringene her',
+    'kvittering.intro.mor.link': '[lenke]', // TODO
+    'kvittering.intro.mor.farSignert': 'Far har {date} erklært farskap for barn', // TODO
+
+    'kvittering.intro.far.1': 'Du har signert farskapserklæring {date} for barn',
+    'kvittering.intro.far.2': 'Erklæringen er endelig og kan ikke endres.',
+    'kvittering.intro.far.3': 'Du kan gjenfinne erklæringer her',
+    'kvittering.intro.far.link': '[lenke]', // TODO
     'kvittering.morAlert': 'Erklæringen er ikke gyldig før oppgitt far har signert. Han kan allerede nå logge seg inn på dittnav og signere erklæringen. Du bør gjøre han oppmerksom på dette. Hvis ikke far har signert etter tre dager vil vi sende han en påminnelse per sms.',
     'kvittering.hvaSkjerVidere.title': 'Hva skjer videre',
     'kvittering.hvaSkjerVidere.1': 'Det er Skatteetaten ved folkeregisteret som har ansvar for å registrere og arkivere farskapserklæringer. Når erklæringen er signert av dere begge vil den derfor bli oversendt til dem. Etter at dere har valgt navn til barnet vil du motta en bekreftelse på registrering av fødselsnummer og navn. Bekreftelsen mottar du i din innboks i Altinn.',
