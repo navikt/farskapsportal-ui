5c888ba2fdf0583095a162a8c625e2d5ac8dbbda
Eilertsen <odin.eilertsen@nav.no>
2021-04-28 12:00:52 +0200

    Visning av feilmelding - fix

--- src/utils/feilkoder.ts
+++ src/utils/feilkoder.ts
@@ -24,6 +24,6 @@ export const isUserNotPermitted = (error: AlertError): boolean =>
     EXPECTED_FEILKODER_FETCH_USER.includes(error.feilkode);
 
 export const isControlFatherValidationError = (error: AlertError): boolean =>
-    error.code === 400 &&
+    (error.code === 400 || error.code === 404) &&
     !!error.feilkode &&
     EXPECTED_FEILKODER_CONTROL_FATHER.includes(error.feilkode);
