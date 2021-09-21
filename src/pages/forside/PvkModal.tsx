1142e7b7ff7a38e4d569b1a78879425e74f988ab
Eilertsen <odin.eilertsen@nav.no>
2021-09-21 20:54:49 +0200

    lagt til tekstelementer i pvk

--- src/pages/forside/PvkModal.tsx
+++ src/pages/forside/PvkModal.tsx
@@ -1,7 +1,9 @@
 import { Knapp } from 'nav-frontend-knapper';
 import Modal from 'nav-frontend-modal';
 import { useState } from 'react';
-import { Normaltekst } from 'nav-frontend-typografi';
+import { Innholdstittel, Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { FormattedMessage } from 'react-intl';
+import FormattedMessageWithExternalLink from 'components/formatted-message-with-external-link/FormattedMessageWithExternalLink';
 
 import './PvkModal.less';
 
@@ -29,7 +31,59 @@ function PvkModal() {
 function Pvk() {
     return (
         <>
-            <Normaltekst>PVK her</Normaltekst>
+            <Innholdstittel>
+                <FormattedMessage id="forside.pvk.title" />
+            </Innholdstittel>
+            <Undertittel>
+                <FormattedMessage id="mor" />
+            </Undertittel>
+            <Normaltekst>
+                <FormattedMessage id="forside.pvk.mor.content" />
+            </Normaltekst>
+            <ul>
+                <li>
+                    <FormattedMessage id="forside.pvk.mor.list.1" />
+                </li>
+                <li>
+                    <FormattedMessage id="forside.pvk.mor.list.2" />
+                </li>
+                <li>
+                    <FormattedMessage id="forside.pvk.mor.list.3" />
+                </li>
+                <li>
+                    <FormattedMessage id="forside.pvk.mor.list.4" />
+                </li>
+            </ul>
+            <Undertittel>
+                <FormattedMessage id="far" />
+            </Undertittel>
+            <Normaltekst>
+                <FormattedMessage id="forside.pvk.far" />
+            </Normaltekst>
+            <ul>
+                <li>
+                    <FormattedMessage id="forside.pvk.far.list.1" />
+                </li>
+                <li>
+                    <FormattedMessage id="forside.pvk.far.list.2" />
+                </li>
+                <li>
+                    <FormattedMessage id="forside.pvk.mor.list.3" />
+                </li>
+            </ul>
+            <Normaltekst>
+                <FormattedMessage id="forside.pvk.etterSignering" />
+            </Normaltekst>
+            <br/>
+            <Undertittel>
+                <FormattedMessage id="forside.pvk.personvernerklaering.title" />
+            </Undertittel>
+            <Normaltekst>
+                <FormattedMessageWithExternalLink
+                    textId="forside.pvk.personvernerklaering.content"
+                    linkId="forside.pvk.personvernerklaering.link"
+                />
+            </Normaltekst>
         </>
     );
 }
