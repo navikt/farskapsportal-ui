b1e8e26836bd56b58e2a25a86ac0fc6cad85dd76
s148719 <jan.kjetil.skar@nav.no>
2023-03-07 13:00:43 +0100

    Redirecte til ENONIC

--- server/server.js
+++ server/server.js
@@ -27,6 +27,11 @@ app.use((req, res, next) => {
 // Static files
 app.use(express.static(buildPath, { index: false }));
 
+app.get('/', (req, res) => res.redirect(${process.env.ENONIC_BOKMAAL}));
+app.get('/nb', (req, res) => res.redirect(${process.env.ENONIC_BOKMAAL}));
+app.get('/nn', (req, res) => res.redirect(${process.env.ENONIC_NYNORSK}));
+app.get('/en', (req, res) => res.redirect(${process.env.ENONIC_ENGELSK}));
+
 // Nais functions
 app.get('/internal/isAlive|isReady', (req, res) => res.sendStatus(200));
 
