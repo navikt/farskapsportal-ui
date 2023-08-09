75199c743371228456b4d034a2490dfee7187ec0
s148719 <jan.kjetil.skar@nav.no>
2023-08-09 14:13:54 +0200

    wonderwall take one

---
+++ server/session.js
@@ -0,0 +1,44 @@
+import session from 'express-session';
+import redis from 'redis';
+import RedisStore from 'connect-redis';
+
+import * as config from './config.js';
+
+export const setupSession = () => {
+    const options = {
+        cookie: {
+            maxAge: config.session.maxAgeMs,
+            sameSite: 'lax',
+            httpOnly: true,
+        },
+        secret: config.session.secret,
+        name: 'farskapsportal-ui',
+        resave: false,
+        saveUninitialized: false,
+        unset: 'destroy',
+    };
+
+    if (process.env.NODE_ENV !== 'development') {
+        options.cookie.secure = true;
+        options.store = setupRedis();
+    }
+
+    return session(options);
+};
+
+const setupRedis = () => {
+    const store = RedisStore(session);
+    const client = redis.createClient({
+        host: config.session.redisHost,
+        password: config.session.redisPassword,
+        port: config.session.redisPort,
+    });
+
+    client.unref();
+    client.on('debug', console.log);
+
+    return new store({
+        client: client,
+        disableTouch: true,
+    });
+};
