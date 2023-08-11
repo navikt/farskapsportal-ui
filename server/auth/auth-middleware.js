56aac165f703af1fcad2fd3ca3c90480e0fd5531
s148719 <jan.kjetil.skar@nav.no>
2023-08-11 12:44:22 +0200

    Update auth-middleware.js

--- server/auth/auth-middleware.js
+++ server/auth/auth-middleware.js
@@ -1,11 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer';
 import 'dotenv/config.js';ß
 
-const validateAccessToken = auth({
+export const validateAccessToken = auth({
     issuerBaseURL: process.env.IDPORTEN_WELL_KNOWN_URL,
     audience: process.env.IDPORTEN_AUDIENCE,
 });
-
-module.exports = {
-    validateAccessToken,
-};
