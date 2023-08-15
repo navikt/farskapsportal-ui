504ef94bb4f81627ac1f1144196776c868eaa2c6
Jan Kjetil Skår <43172034+s148719@users.noreply.github.com>
2023-08-15 08:41:44 +0200

    BD-1703: Fjerne CORS headers. (#313)
    * BD-1703: Fjerne CORS headers.
    * Fjerne utkommenterte Enonic-redirects

--- server/headers.js
+++ server/headers.js
@@ -5,9 +5,6 @@ export const setup = (app) => {
         res.header('X-Xss-Protection', '1; mode=block');
         res.header('X-Content-Type-Options', 'nosniff');
         res.header('Referrer-Policy', 'no-referrer');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     });
 };
