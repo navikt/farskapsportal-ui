c574ddb888ce0877f126c5e608f84317466a86c5
s148719 <jan.kjetil.skar@nav.no>
2023-08-15 06:56:03 +0200

    BD-1703: Legge til CORS manuelt

--- server/headers.js
+++ server/headers.js
@@ -1,20 +1,13 @@
-import cors from 'cors';
-
 export const setup = (app) => {
     app.disable('x-powered-by');
-    app.use(cors());
     app.use((req, res, next) => {
         res.header('X-Frame-Options', 'DENY');
         res.header('X-Xss-Protection', '1; mode=block');
         res.header('X-Content-Type-Options', 'nosniff');
         res.header('Referrer-Policy', 'no-referrer');
-
-        if (process.env.ENV === 'prod') {
-            res.setHeader('Access-Control-Allow-Origin', 'https://farskapsportal.nav.no');
-            res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
-            res.setHeader('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Accept');
-        }
-
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     });
 };
